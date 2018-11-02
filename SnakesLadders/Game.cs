bc47869f1aaa8f9253070052eceafd6e8d0f5c97
skirmamack <skirmamack@yahoo.com>
2018-11-02 15:48:42 +0000

    Player can win

--- SnakesLadders/Game.cs
+++ SnakesLadders/Game.cs
@@ -1,7 +1,11 @@
-﻿namespace SnakesLadders
+﻿using System.Collections.Generic;
+
+namespace SnakesLadders
 {
     public class Game
     {
+        private const int MaxSquareNumber = 100;
+
         private int _tokenSquareNumber;
         private Token _token;
 
@@ -18,12 +22,27 @@
         public void MoveToken(Token token, int diceNumber)
         {
             var newSquareNumber = _tokenSquareNumber + diceNumber;
+
+            if (newSquareNumber > MaxSquareNumber) return;
+
             _tokenSquareNumber = newSquareNumber;
         }
 
         public int GetTokenSquareNumber()
         {
             return _tokenSquareNumber;
+        }
+
+        public List<Token> GetWinnerTokens()
+        {
+            var tokens = new List<Token>();
+
+            if (_tokenSquareNumber == MaxSquareNumber)
+            {
+                tokens.Add(_token);
+            }
+
+            return tokens;
         }
     }
 }
--- SnakesLaddersTests.Unit/GameTests.cs
+++ SnakesLaddersTests.Unit/GameTests.cs
@@ -66,5 +66,64 @@ namespace SnakesLaddersTests.Unit
             tokenSquareNumber = _game.GetTokenSquareNumber();
             Assert.That(tokenSquareNumber, Is.EqualTo(8));
         }
+
+        [Test]
+        public void Player_CanWin_When_SteppedOnTheLastSquare()
+        {
+            //starts at 1st
+            var token = new Token("Ski");
+            _game.AddToken(token);
+            _game.Start();
+
+            var winnerTokens = _game.GetWinnerTokens();
+            CollectionAssert.IsEmpty(winnerTokens);
+
+            //Given the token is on square 97
+            MoveTokenToSquareNumber(token, 97);
+
+            //When the token is moved 3 spaces
+            _game.MoveToken(token, 3);
+
+            //Then the token is on square 100
+            var tokenSquareNumber = _game.GetTokenSquareNumber();
+            Assert.That(tokenSquareNumber, Is.EqualTo(100));
+
+            //And the player has won the game
+            winnerTokens = _game.GetWinnerTokens();
+            CollectionAssert.Contains(winnerTokens, token);
+        }
+
+        [Test]
+        public void Player_DidNotWin_When_SteppedOverLastSquare()
+        {
+            var token = new Token("Ski");
+            _game.AddToken(token);
+            _game.Start();
+
+            var winnerTokens = _game.GetWinnerTokens();
+            CollectionAssert.IsEmpty(winnerTokens);
+
+            //Given the token is on square 97
+            MoveTokenToSquareNumber(token, 97);
+
+            //When the token is moved 4 spaces
+            _game.MoveToken(token, 4);
+
+            //Then the token is on square 97
+            var tokenSquareNumber = _game.GetTokenSquareNumber();
+            Assert.That(tokenSquareNumber, Is.EqualTo(97));
+
+            //And the player has not won the game
+            winnerTokens = _game.GetWinnerTokens();
+            CollectionAssert.IsEmpty(winnerTokens);
+        }
+
+        protected void MoveTokenToSquareNumber(Token token, int squareNumber)
+        {
+            while (_game.GetTokenSquareNumber() < squareNumber)
+            {
+                _game.MoveToken(token, 1);
+            }
+        }
     }
 }
